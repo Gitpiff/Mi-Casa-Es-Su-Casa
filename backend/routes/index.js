9870c3df030e8fd6388093e9f2e5654d52fdd1e2
Victor <viknavarro@gmail.com>
2023-12-22 21:32:14 +0000

    backend deployed to render

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
-const { restoreUser } = require('../utils/auth');
+//const { restoreUser } = require('../utils/auth');
 
 router.use('/api', apiRouter);
 
