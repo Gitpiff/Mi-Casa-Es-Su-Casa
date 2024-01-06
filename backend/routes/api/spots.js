22599e6222ce8144d36203c944b9a88887d66a28
Victor <viknavarro@gmail.com>
2024-01-06 22:49:51 +0000

    initial spots route commit

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,15 +2,25 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
-
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { Spot } = require('../../db/models');
+const Review = require('../../db/models/review');
+const Spotimage = require('../../db/models/spotimage');
 
 const router = express.Router();
 
 //Get All Spots
 router.get('/', async (req, res) => {
-    const spots = await Spot.findAll()
+    const spots = await Spot.findAll({
+        include: [
+            {
+                model: Review
+            },
+            {
+                model: Spotimage
+            }
+        ]
+    })
 
     return res.json({
         spots
@@ -30,6 +40,27 @@ router.get('/current', requireAuth, async (req, res) => {
     return res.json({
         currentUserSpots
     })
+});
+
+//Get details of a Spot from an id
+router.get('/:spotId', async (req, res) => {
+    const { spotId } = req.params;
+    //console.log(spotId)
+    try {
+        const spot = await Spot.findByPk(parseInt(spotId));
+
+        if(!spot) return res.status(404).json(
+            {
+                message: "Spot couldn't be found"
+              }
+        )
+
+        return res.json({
+            spot
+        })
+    } catch(error) {
+        console.log(error)
+    }
 });
 
 module.exports = router;
