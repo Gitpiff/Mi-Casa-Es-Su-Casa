fe64497d33fb41a2bc83ea035cf4cb018a11a295
Victor <viknavarro@gmail.com>
2024-01-12 00:13:50 +0000

    review.userId column error

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -11,16 +11,18 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Review.belongsTo(models.User, {
-        foreignKey: 'userId'
-      });
-
       Review.belongsTo(models.Spot, {
         foreignKey: 'spotId'
       });
 
+      Review.belongsTo(models.User, {
+        foreignKey: 'userId'
+      });
+
       Review.hasMany(models.ReviewImage, {
-        foreignKey: 'reviewId'
+        foreignKey: 'reviewId',
+        onDelete: 'CASCADE',
+        hooks: true
       });
     }
   }
