ac92b5581fa9ce309d663aeefe13c460250ee4ff
Victor <viknavarro@gmail.com>
2024-01-12 00:27:42 +0000

    review.userId column error

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -29,11 +29,17 @@ module.exports = (sequelize, DataTypes) => {
     },
     spotId: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      references: {
+        model: 'Spots'
+      }
     },
     userId: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      references: {
+        model: 'Users'
+      }
     },
     startDate: {
       type: DataTypes.DATE,
