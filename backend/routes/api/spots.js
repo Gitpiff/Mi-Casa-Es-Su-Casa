6a0f7bc750fe8959c6d07fc6fdaad59bc20a8a25
Victor <viknavarro@gmail.com>
2024-01-13 22:42:50 +0000

    Fixed Get all Reviews enpoint in production

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -108,7 +108,7 @@ router.get('/current', requireAuth, async (req, res) => {
                     attributes: ['stars'],
                 },
                 {
-                    model: Image,
+                    model: ReviewImage,
                     attributes: ['url', 'preview'],
                 }
             ],
