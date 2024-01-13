8616640853d279eae23dde467aaf441b5b9f73b0
Victor <viknavarro@gmail.com>
2024-01-13 18:18:55 +0000

    get all reviews working locally

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,57 +1,55 @@
 const express = require('express');
 
 const { requireAuth } = require('../../utils/auth');
-const { Spot, Review, Image, User } = require('../../db/models');
+const { Spot, Review, ReviewImage, User } = require('../../db/models');
 
 const router = express.Router();
 
 //Get all Reviews of the Current User
 router.get('/current', requireAuth, async (req, res, next) => {
-    let data = {}
-    let currUser = +req.user.id
-    let reviews = await Review.findAll({
+    const { user } = req;
+
+    const reviews = await Review.findAll({
         where: {
-            userId: currUser
+            userId: user.id
         },
         include: [
-            {
-                model: Spot,
-                include: [
-                    {
-                        model: Image,
-                        as: 'SpotImages'
-                    }
-                ]
-            },
-            {
+            { 
                 model: User,
                 attributes: ['id', 'firstName', 'lastName']
             },
             {
-                model: Image,
-                as: 'ReviewImages',
+                model: Spot,
+                // attributes: {
+                //     exclude: ['description', 'createdAt', 'updatedAt']
+                // }
+            },
+            {
+                model: ReviewImage,
                 attributes: ['id', 'url']
             }
-        ],
-    })
-    data = reviews.map(review => review.toJSON());
-
-    data.forEach(review => {
-        if (review.ReviewImages.length === 0) {
-            review.ReviewImages = {
-                message: 'no images to display'
-            }
-        }
-        if (review.Spot.SpotImages.length === 0) {
-            review.Spot.previewImage = 'no images url'
-        } else {
-            review.Spot.SpotImages.forEach(image => {
-                review.Spot.previewImage = image.url
-            })
-        }
-        delete review.Spot.SpotImages
-        delete review.Spot.createdAt
-        delete review.Spot.updatedAt
+        ]
     });
-    res.json({ Reviews: data })
+
+    const Reviews = [];
+
+    reviews.forEach(review => {
+        Reviews.push(review.toJSON())
+    });
+
+    Reviews.forEach(review => {
+        review.ReviewImages.forEach(image => {
+            if(image.preview === true) {
+                review.Spot.previewImage = image.url
+            }
+        });
+        delete review.Spot.SpotImage;
+        //console.log(review.ReviewImages)
+
+    });
+
+    res.json(Reviews)
 });
+
+
+module.exports = router;
