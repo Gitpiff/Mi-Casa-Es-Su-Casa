b27013220656d0c6eca475ed61ab45a18995fc39
Victor <viknavarro@gmail.com>
2024-01-15 00:51:33 +0000

    refactored spots and reviews, I think it's done

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -313,7 +313,7 @@ router.get('/current', requireAuth, async (req, res) => {
                 }
             });
 
-            delete spot.Images;
+            delete spot.spotImages;
             delete spot.Reviews;
         });
 
