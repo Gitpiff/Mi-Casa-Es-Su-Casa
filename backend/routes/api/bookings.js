3b04130bf2c3b9293ff91b2fbd7aefdf62b5fc79
Victor <viknavarro@gmail.com>
2024-01-15 23:05:29 +0000

    added parsefloat to lat, lng and price

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -51,9 +51,9 @@ router.get('/current', requireAuth, async (req, res, next) => {
     });
 
     bookingsArr.forEach(booking => {
-        booking.Spot.lat = Number(booking.Spot.lat);
-        booking.Spot.lng = Number(booking.Spot.lng);
-        booking.Spot.price = Number(booking.Spot.price);
+        booking.Spot.lat = Number.parseFloat(booking.Spot.lat);
+        booking.Spot.lng = Number.parseFloat(booking.Spot.lng);
+        booking.Spot.price = Number,parseFloat(booking.Spot.price);
 
         booking.Spot.SpotImages.forEach(image => {
             if (image.preview === true) {
