6b927766e845edb7b27e8cd401fd0a1086754d57
Victor <viknavarro@gmail.com>
2024-02-10 19:55:42 +0000

    Authenticate me phase 3 navigation component

---
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -0,0 +1,11 @@
+import { FaUserAstronaut } from "react-icons/fa";
+
+function ProfileButton() {
+    return ( 
+        <div style={{fontSize: "50px"}}>
+            <FaUserAstronaut />
+        </div>
+     );
+}
+
+export default ProfileButton;
